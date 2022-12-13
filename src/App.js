56aa00dc5905004099f81f5e1158632ddeccdb08
HughesSeanKyle <khughessean@yahoo.com>
2022-12-13 17:33:54 +0200

    Navbar and light/dark logic

--- src/App.js
+++ src/App.js
@@ -2,26 +2,18 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './App.css';
 import { Button } from 'reactstrap';
+import NavBar from './components/NavBar';
 
 function App() {
 	const [selectedColorMode, setSelectedColorMode] = useState(null);
+
+	const readGlobalState = { selectedColorMode };
+	const writeGlobalState = { setSelectedColorMode };
 
 	const setInitColorMode = () => {
 		let colorMode = JSON.parse(localStorage.getItem('colorMode'));
 
 		if (!colorMode) {
-			localStorage.setItem('colorMode', JSON.stringify('light'));
-			setSelectedColorMode('light');
-			return;
-		}
-	};
-
-	const onColorModeChange = () => {
-		if (selectedColorMode === 'light') {
-			localStorage.setItem('colorMode', JSON.stringify('dark'));
-			setSelectedColorMode('dark');
-			return;
-		} else {
 			localStorage.setItem('colorMode', JSON.stringify('light'));
 			setSelectedColorMode('light');
 			return;
@@ -35,19 +27,12 @@ function App() {
 	return (
 		<div className={selectedColorMode === 'light' ? 'app-light' : 'app-dark'}>
 			<BrowserRouter>
+				<NavBar
+					readGlobalState={readGlobalState}
+					writeGlobalState={writeGlobalState}
+				/>
 				<Routes>
-					<Route
-						path="/"
-						element={
-							<div>
-								<Button onClick={() => onColorModeChange()}>
-									{selectedColorMode === 'light'
-										? 'Switch to dark mode'
-										: 'Switch to light mode'}
-								</Button>
-							</div>
-						}
-					/>
+					<Route path="/" element={<div>All flags</div>} />
 					<Route path="/flag/:flagId" element={<div>Flag by ID</div>} />
 				</Routes>
 			</BrowserRouter>
