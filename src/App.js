737a2e91373f33e426c9935069e65b0c962e07ff
HughesSeanKyle <khughessean@yahoo.com>
2022-12-13 15:09:53 +0200

    Handle initial color mode set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+	const [selectedColorMode, setSelectedColorMode] = useState(null);
+
+	useEffect(() => {
+		let colorMode = JSON.parse(localStorage.getItem('colorMode'));
+
+		if (!colorMode) {
+			localStorage.setItem('colorMode', JSON.stringify('light'));
+			setSelectedColorMode('light');
+			return;
+		}
+
+		setSelectedColorMode(colorMode);
+	}, [selectedColorMode]);
+
 	return (
-		<div className={colorMode === 'light' ? 'app-light' : 'app-dark'}>
+		<div className={selectedColorMode === 'light' ? 'app-light' : 'app-dark'}>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<div>All flags</div>} />
