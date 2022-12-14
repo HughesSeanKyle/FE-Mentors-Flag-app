2006303690909e5cd8c4df4fea3f407ae722310f
HughesSeanKyle <khughessean@yahoo.com>
2022-12-14 10:22:54 +0200

    Get flags api data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { Button } from 'reactstrap';
+
+import { getAllCountries } from './api/getCountries';
 import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
 	const [selectedColorMode, setSelectedColorMode] = useState(null);
+	const [allCountries, setAllCountries] = useState(null);
 
-	const readGlobalState = { selectedColorMode };
-	const writeGlobalState = { setSelectedColorMode };
+	const readGlobalState = { selectedColorMode, allCountries };
+	const writeGlobalState = { setSelectedColorMode, setAllCountries };
+
+	console.log('readGlobalState', readGlobalState);
 
 	const setInitColorMode = () => {
 		let colorMode = JSON.parse(localStorage.getItem('colorMode'));
@@ -22,6 +26,15 @@ function App() {
 
 	useEffect(() => {
 		setInitColorMode();
+		(async () => {
+			const allCountriesResp = await getAllCountries();
+
+			if (allCountriesResp.data) {
+				setAllCountries(allCountriesResp.data);
+			} else {
+				setAllCountries(allCountriesResp.error);
+			}
+		})();
 	}, []);
 
 	return (
@@ -33,7 +46,7 @@ function App() {
 				/>
 				<Routes>
 					<Route path="/" element={<div>All flags</div>} />
-					<Route path="/flag/:flagId" element={<div>Flag by ID</div>} />
+					<Route path="/country/:name" element={<div>Flag by ID</div>} />
 				</Routes>
 			</BrowserRouter>
 		</div>
