7edfed3becf8a99caad7e0ff9604fed40da6f8e6
HughesSeanKyle <khughessean@yahoo.com>
2022-12-15 12:13:22 +0200

    Update search logic

--- src/components/forms/SearchBar.jsx
+++ src/components/forms/SearchBar.jsx
@@ -12,48 +12,44 @@ const SearchBar = ({
 	const { selectedColorMode, allCountries } = readGlobalState;
 
 	// Helper
-	const firstCharToUpperCase = (string) => {
-		if (!string) {
-			return null;
+	const firstCharToUpperCase = (term) => {
+		if (!term) {
+			return '';
 		}
-		return string[0].toUpperCase() + string.slice(1).toLowerCase();
+		return term[0].toUpperCase() + term.slice(1).toLowerCase();
 	};
 
 	return (
-		<Form className="ml-lg-auto mr-3 mt-4">
-			<FormGroup className="mb-0 search-bar-dimensions">
-				<InputGroup className="input-group-alternative">
-					<InputGroupText
-						className={
-							selectedColorMode === 'light'
-								? 'search-bar__icon-light'
-								: 'search-bar__icon-dark'
-						}
-					>
-						<i
-							style={
-								selectedColorMode === 'light'
-									? { color: 'hsl(200, 15%, 8%)' }
-									: { color: 'hsl(0, 0%, 100%)' }
-							}
-							className="fa fa-search"
-						/>
-					</InputGroupText>
-					<Input
-						className={
-							selectedColorMode === 'light'
-								? 'search-bar-light'
-								: 'search-bar-dark search-filter__text-light'
-						}
-						placeholder="Search"
-						type="text"
-						onChange={(e) =>
-							onSearchOrFilterUpdate(firstCharToUpperCase(e.target.value))
-						}
-					/>
-				</InputGroup>
-			</FormGroup>
-		</Form>
+		<InputGroup className="input-group-alternative search-bar-dimensions">
+			<InputGroupText
+				className={
+					selectedColorMode === 'light'
+						? 'search-bar__icon-light'
+						: 'search-bar__icon-dark'
+				}
+			>
+				<i
+					style={
+						selectedColorMode === 'light'
+							? { color: 'hsl(200, 15%, 8%)' }
+							: { color: 'hsl(0, 0%, 100%)' }
+					}
+					className="fa fa-search"
+				/>
+			</InputGroupText>
+			<Input
+				className={
+					selectedColorMode === 'light'
+						? 'search-bar-light'
+						: 'search-bar-dark search-filter__text-light'
+				}
+				placeholder="Search"
+				type="text"
+				onChange={(e) =>
+					onSearchOrFilterUpdate(firstCharToUpperCase(e.target.value))
+				}
+			/>
+		</InputGroup>
 	);
 };
 
