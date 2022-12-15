c6e72e804f4bc450f2a7919855713157889b055d
HughesSeanKyle <khughessean@yahoo.com>
2022-12-15 08:40:22 +0200

    Filter select basic logic

--- src/components/forms/FilterSelectBar.jsx
+++ src/components/forms/FilterSelectBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { Form, FormGroup, InputGroupText, Input, InputGroup } from 'reactstrap';
 
@@ -7,6 +7,10 @@ import '../../App.css';
 const FilterSelectBar = ({ readGlobalState, writeGlobalState }) => {
 	const { selectedColorMode, allCountries } = readGlobalState;
 
+	const [selectedField, setSelectedField] = useState('Filter by Region');
+
+	console.log('selectedField', selectedField);
+
 	return (
 		<Form className="ml-lg-auto mr-3 mt-4">
 			<FormGroup className="mb-0 filter-bar-dimensions">
@@ -14,18 +18,75 @@ const FilterSelectBar = ({ readGlobalState, writeGlobalState }) => {
 					<Input
 						className={
 							selectedColorMode === 'light'
-								? 'search-bar-light'
-								: 'search-bar-dark'
+								? 'search-bar-light search-filter__text-light'
+								: 'search-bar-dark search-filter__text-dark'
 						}
 						placeholder="Filter"
 						type="select"
+						onChange={(e) => setSelectedField(e.target.value)}
 					>
-						<option value={'option1'}>Filter by Region</option>
-						<option value={'option1'}>Africa</option>
-						<option value={'option2'}>America</option>
-						<option value={'option1'}>Asia</option>
-						<option value={'option2'}>Europe</option>
-						<option value={'option1'}>Oceana</option>
+						<option
+							className={
+								selectedColorMode === 'light'
+									? 'search-filter__text-light'
+									: 'search-filter__text-dark'
+							}
+							value={'Filter by Region'}
+						>
+							{selectedField !== 'Filter by Region'
+								? 'All'
+								: 'Filter by Region'}
+						</option>
+						<option
+							className={
+								selectedColorMode === 'light'
+									? 'search-filter__text-light'
+									: 'search-filter__text-dark'
+							}
+							value={'Africa'}
+						>
+							Africa
+						</option>
+						<option
+							className={
+								selectedColorMode === 'light'
+									? 'search-filter__text-light'
+									: 'search-filter__text-dark'
+							}
+							value={'America'}
+						>
+							America
+						</option>
+						<option
+							className={
+								selectedColorMode === 'light'
+									? 'search-filter__text-light'
+									: 'search-filter__text-dark'
+							}
+							value={'Asia'}
+						>
+							Asia
+						</option>
+						<option
+							className={
+								selectedColorMode === 'light'
+									? 'search-filter__text-light'
+									: 'search-filter__text-dark'
+							}
+							value={'Europe'}
+						>
+							Europe
+						</option>
+						<option
+							className={
+								selectedColorMode === 'light'
+									? 'search-filter__text-light'
+									: 'search-filter__text-dark'
+							}
+							value={'Oceana'}
+						>
+							Oceana
+						</option>
 					</Input>
 				</InputGroup>
 			</FormGroup>
