249e274869e16199d4d90409395c406546d8ba33
HughesSeanKyle <khughessean@yahoo.com>
2022-12-26 11:01:31 +0200

    Update code

--- src/components/forms/SearchBar.jsx
+++ src/components/forms/SearchBar.jsx
@@ -20,39 +20,44 @@ const SearchBar = ({
 	};
 
 	return (
-		<InputGroup className="input-group-alternative search-bar-dimensions">
-			<InputGroupText
-				className={
-					selectedColorMode === 'light'
-						? 'search-bar__icon-light'
-						: 'search-bar__icon-dark'
-				}
-			>
-				<i
-					style={
+		<>
+			<div class="search-container">
+				<input type="text" placeholder="Search..." />
+			</div>
+			<InputGroup className="input-group-alternative search-bar-dimensions">
+				<InputGroupText
+					className={
 						selectedColorMode === 'light'
-							? { color: 'hsl(200, 15%, 8%)' }
-							: { color: 'hsl(0, 0%, 100%)' }
+							? 'search-bar__icon-light'
+							: 'search-bar__icon-dark'
 					}
-					className="fa fa-search"
+				>
+					<i
+						style={
+							selectedColorMode === 'light'
+								? { color: 'hsl(200, 15%, 8%)' }
+								: { color: 'hsl(0, 0%, 100%)' }
+						}
+						className="fa fa-search"
+					/>
+				</InputGroupText>
+				<Input
+					className={
+						selectedColorMode === 'light'
+							? 'search-bar-light'
+							: 'search-bar-dark search-filter__text-light'
+					}
+					placeholder="Search for a country..."
+					type="text"
+					onChange={(e) =>
+						onSearchOrFilterUpdate(
+							'SearchBar',
+							firstCharToUpperCase(e.target.value)
+						)
+					}
 				/>
-			</InputGroupText>
-			<Input
-				className={
-					selectedColorMode === 'light'
-						? 'search-bar-light'
-						: 'search-bar-dark search-filter__text-light'
-				}
-				placeholder="Search for a country..."
-				type="text"
-				onChange={(e) =>
-					onSearchOrFilterUpdate(
-						'SearchBar',
-						firstCharToUpperCase(e.target.value)
-					)
-				}
-			/>
-		</InputGroup>
+			</InputGroup>
+		</>
 	);
 };
 
